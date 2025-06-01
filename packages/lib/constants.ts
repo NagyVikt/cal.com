3bf26e9047bd0f19b089ba846f5d5a1d2c0744a2
NagyVikt <nagy.viktordp@gmail.com>
2025-06-01 12:53:19 +0200

    parse

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -117,7 +117,10 @@ export const FULL_NAME_LENGTH_MAX_LIMIT = 50;
 export const MINUTES_TO_BOOK = process.env.NEXT_PUBLIC_MINUTES_TO_BOOK || "5";
 export const ENABLE_PROFILE_SWITCHER = process.env.NEXT_PUBLIC_ENABLE_PROFILE_SWITCHER === "1";
 // Needed for orgs
+console.log("ALLOWED_HOSTNAMES ENV:", process.env.ALLOWED_HOSTNAMES);
+console.log("ALLOWED_HOSTNAMES FOR JSON:", `[${process.env.ALLOWED_HOSTNAMES || ""}]`);
 export const ALLOWED_HOSTNAMES = JSON.parse(`[${process.env.ALLOWED_HOSTNAMES || ""}]`) as string[];
+
 export const RESERVED_SUBDOMAINS = JSON.parse(`[${process.env.RESERVED_SUBDOMAINS || ""}]`) as string[];
 
 export const ORGANIZATION_SELF_SERVE_MIN_SEATS = parseInt(
